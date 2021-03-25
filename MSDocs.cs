456e52136355d1e621c480c6a9cd4574e49e14f4
WomackCodes <chris@womack.codes>
2021-03-24 22:51:53 -0700

    new git repo made

---
+++ MSDocs.cs
@@ -0,0 +1,52 @@
+// string hello = "Hello ";
+// string world = "World";
+
+// Console.WriteLine(hello + world + $" in C#!");
+
+// string firstPlanet = "Mercury";
+// string secondCareer = "Venus";
+
+// Console.WriteLine();
+
+
+// string greeting = "    Hello World.    ";
+// Console.WriteLine($"[{greeting}]");
+
+// string trimmedGreeting = greeting.TrimStart();
+// Console.WriteLine($"[{trimmedGreeting}]");
+
+// trimmedGreeting = greeting.TrimEnd();
+// Console.WriteLine($"[{trimmedGreeting}]");
+
+// trimmedGreeting = greeting.Trim();
+// Console.WriteLine($"[{trimmedGreeting}");
+
+// string sayHello = "Hello World!";
+// Console.WriteLine(sayHello);
+// sayHello = sayHello.Replace("Hello", "Greetings");
+// Console.WriteLine(sayHello);
+
+// string songLyrics = "You say goodbye, and I say hello";
+// Console.WriteLine(songLyrics.Contains("goodbye"));
+// Console.WriteLine(songLyrics.Contains("greetings"));
+
+// string songLyrics = "You say goodbye, and I say hello";
+// Console.WriteLine(songLyrics.StartsWith("You"));
+// Console.WriteLine(songLyrics.StartsWith("goodbye"));
+
+// Console.WriteLine(songLyrics.EndsWith("hello"));
+// Console.WriteLine(songLyrics.EndsWith("goodbye"));
+
+// int a = 18;
+// int b = 6;
+// int c = a + b;
+// int d = a * b;
+// int c = a / b;
+
+
+
+// Console.WriteLine(c);
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
